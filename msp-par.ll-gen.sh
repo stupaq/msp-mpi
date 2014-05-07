f9d34f2144e2b4f34320ac4b41f5f2cebea2aea4
Mateusz Machalica <mateuszmachalica@gmail.com>
2014-05-07 22:35:49 +0200

    LoadLeveler description file and generator.

---
+++ msp-par.ll-gen.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+N=${N:-1000}
+M=${M:-1000}
+P=${P:-32}
+C=${C:-4}
+S=${S:-123}
+
+case $C in
+  1) mode=SMP ;;
+  2) mode=DUAL ;;
+  4) mode=VN ;;
+esac
+
+cat <<EOF
+# @ job_name = MSP_`whoami`_${M}x${N}
+# @ account_no = G52-5
+# @ class = kdm
+# @ error = MSP_`whoami`_${M}x${N}.err
+# @ output = MSP_`whoami`_${M}x${N}.out
+# @ environment = COPY_ALL
+# @ wall_clock_limit = 00:15:00
+# @ notification = error
+# @ notify_user = `whoami`@students.mimuw.edu.pl
+# @ job_type = bluegene
+# @ bg_size = `echo $(($P / $C))`
+# @ queue
+echo "Started at" \`date\`
+mpirun -exe msp-par.exe -np 32 -mode ${mode} -args ${M} ${N} ${S}
+echo "Finished at" \`date\`
+EOF
