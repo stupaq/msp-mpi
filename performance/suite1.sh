7a3e88fa8a45e8c550ce22981008d7b2f3f12e7c
Mateusz Machalica <mateuszmachalica@gmail.com>
2014-05-10 13:41:53 +0200

    Performance test cases.

---
+++ performance/suite1.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+SUBMIT="`dirname $0`/../submit.sh"
+REPORT="`dirname $0`/../perf-report.sh"
+ERRS="`dirname $SUBMIT`/*.err"
+
+seed=123
+
+rm -f $ERRS
+for m in 1000 2000 4000; do
+  for n in 1000 2000 4000; do
+    echo "Submitting M=$m N=$n S=$seed"
+    if [[ m -eq 4000 && n -eq 4000 ]]; then
+      extra='-A -s'
+    fi
+    $SUBMIT -m $m -n $n -s $seed -p 4 $extra
+  done
+done
+$REPORT $ERRS
