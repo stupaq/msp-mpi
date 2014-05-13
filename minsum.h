7111cd728d127054d354c8a1f018f9ee04413137
Mateusz Machalica <mateuszmachalica@gmail.com>
2014-05-13 22:55:24 +0200

    Ok this one looks ok.

--- minsum.h
+++ minsum.h
@@ -3,6 +3,9 @@
 #ifndef MINSUM_H_
 #define MINSUM_H_
 
+#include <assert.h>
+#include <stdbool.h>
+#include <math.h>
 #include "./ranking.h"
 
 #define LIST_ARR(_i_, _j_) list_ptr[(_i_) * j_count + (_j_)]
@@ -35,6 +38,7 @@ static inline void minsum_prepare(
   }
 }
 
+// TODO(stupaq) this is a modification of the new algorithm, needs proof
 static inline void minsum_find_one(
     long long (*A) (int, int),              /* i_count x k_count */
     long long (*B) (int, int),              /* k_count x j_count */
@@ -43,7 +47,61 @@ static inline void minsum_find_one(
     long long* restrict result_sum,         /* j_count */
     int* restrict result_k                  /* j_count */
     ) {
-  // TODO
+  void* const temp_ptr = malloc(k_count * (sizeof(int) + sizeof(long long)) +
+      j_count * sizeof(bool));
+  int* cand = temp_ptr;                               /* k_count */
+  memset(cand, 0, k_count);
+#define CAND_GET(k) LIST_ARR(k, cand[k])
+#define CAND_NEXT(k) LIST_ARR(k, ++cand[k])
+#define CAND_HAS(k) (cand[k] < j_count)
+  bool* solved = (bool*) (cand + k_count);            /* j_count */
+  memset(solved, 0, j_count);
+  long long* dist = (long long*) (solved + j_count);  /* k_count */
+  struct Ranking queue;                               /* k_count */
+  ranking_create(&queue, k_count, k_count);
+  queue.size_ = k_count;
+  for (int k = 0; k < k_count; ++k) {
+    queue.key_[k] = k;
+    dist[k] = A(i_ind, k) + B(k, CAND_GET(k));
+    queue.value_[k] = dist[k];
+  }
+  ranking_rebuild(&queue);
+  int solved_count = 0;
+  const double offset = k_count + k_count / log((double) k_count);
+  while (solved_count < j_count) {
+    int k = ranking_min_key(&queue);
+    long long d_k = ranking_min_value(&queue);
+    ranking_pop(&queue);
+    int j = CAND_GET(k);
+    assert(CAND_HAS(k));
+    assert(d_k == dist[k]);
+    assert(d_k == A(i_ind, k) + B(k, j));
+    if (!solved[j]) {
+      ++solved_count;
+      solved[j] = true;
+      result_sum[j] = d_k;
+      result_k[j] = k;
+      /* Update for j makes no sense. */
+    }
+    /* The update for k. */
+    int w = CAND_GET(k);
+    int limit = (int) ((double) k_count / (offset - solved_count)) + 1;
+    for (; solved[w] && limit >= 0; --limit) {
+      w = CAND_NEXT(k);
+      assert(CAND_HAS(k));
+    }
+    long long new_dist = A(i_ind, k) + B(k, w);
+    dist[w] = (new_dist < dist[w]) ? new_dist : dist[w];
+    if (ranking_contains(&queue, k)) {
+      ranking_increase(&queue, k, new_dist);
+    } else {
+      ranking_push(&queue, k, new_dist);
+    }
+  }
+#undef CAND_NEXT
+#undef CAND_GET
+  ranking_free(&queue);
+  free(temp_ptr);
 }
 
 #undef LIST_ARR
--- ranking.h
+++ ranking.h
@@ -31,23 +31,23 @@ struct Ranking {
   RankingValue* value_;
 };
 
-inline bool ranking_empty(struct Ranking* restrict heap) {
+static inline bool ranking_empty(struct Ranking* restrict heap) {
   return heap->size_ == 0;
 }
 
-inline bool ranking_contains(struct Ranking* restrict heap, int key) {
+static inline bool ranking_contains(struct Ranking* restrict heap, int key) {
   return heap->position_[key] >= 0;
 }
 
-inline int ranking_min_key(struct Ranking* restrict heap) {
+static inline int ranking_min_key(struct Ranking* restrict heap) {
   return heap->key_[0];
 }
 
-inline RankingValue ranking_min_value(struct Ranking* restrict heap) {
+static inline RankingValue ranking_min_value(struct Ranking* restrict heap) {
   return heap->value_[0];
 }
 
-inline void ranking_swap_entries(struct Ranking* restrict heap, int i,
+static inline void ranking_swap_entries(struct Ranking* restrict heap, int i,
     int j) {
   assert(heap->size_ > i && i >= 0);
   assert(heap->size_ > j && j >= 0);
@@ -59,7 +59,7 @@ inline void ranking_swap_entries(struct Ranking* restrict heap, int i,
   heap->position_[heap->key_[j]] = j;
 }
 
-inline void ranking_heapify(struct Ranking* restrict heap, const int i) {
+static inline void ranking_heapify(struct Ranking* restrict heap, const int i) {
   assert(heap->size_ > i && i >= 0);
   const int l = LEFT(i), r = RIGHT(i);
   int j = i;
@@ -75,12 +75,12 @@ inline void ranking_heapify(struct Ranking* restrict heap, const int i) {
   }
 }
 
-inline void ranking_free(struct Ranking* restrict heap) {
+static inline void ranking_free(struct Ranking* restrict heap) {
   free(heap->key_);
   RESET_HEAP(heap);
 }
 
-inline int ranking_create(struct Ranking* restrict heap, const int capacity,
+static inline int ranking_create(struct Ranking* restrict heap, const int capacity,
     const int keys_range) {
   unsigned char* ptr = (unsigned char*) malloc(capacity * sizeof(int) +
       capacity * sizeof(RankingValue) + keys_range * sizeof(int));
@@ -98,7 +98,7 @@ inline int ranking_create(struct Ranking* restrict heap, const int capacity,
   return 0;
 }
 
-inline void ranking_rebuild(struct Ranking* restrict heap) {
+static inline void ranking_rebuild(struct Ranking* restrict heap) {
   assert(heap->size_ >= 0);
   for (int i = 0; i < heap->size_; ++i) {
     heap->position_[heap->key_[i]] = i;
@@ -108,7 +108,7 @@ inline void ranking_rebuild(struct Ranking* restrict heap) {
   }
 }
 
-inline void ranking_push(struct Ranking* restrict heap, int key, RankingValue
+static inline void ranking_push(struct Ranking* restrict heap, int key, RankingValue
     value) {
   assert(heap->size_ >= 0);
   assert(!ranking_contains(heap, key));
@@ -127,7 +127,7 @@ inline void ranking_push(struct Ranking* restrict heap, int key, RankingValue
   }
 }
 
-inline void ranking_pop(struct Ranking* restrict heap) {
+static inline void ranking_pop(struct Ranking* restrict heap) {
   assert(heap->size_ > 0);
   --heap->size_;
   heap->position_[heap->key_[0]] = -1;
@@ -139,7 +139,7 @@ inline void ranking_pop(struct Ranking* restrict heap) {
   }
 }
 
-inline void ranking_increase(struct Ranking* restrict heap, int key,
+static inline void ranking_increase(struct Ranking* restrict heap, int key,
     RankingValue new_value) {
   assert(ranking_contains(heap, key));
   int i = heap->position_[key];
