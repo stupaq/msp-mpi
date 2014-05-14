97a534408c10b3a557f69f542092ba73bd2c6348
Mateusz Machalica <mateuszmachalica@gmail.com>
2014-05-15 01:37:54 +0200

    Time comparator.

--- compare-seq.sh
+++ compare-seq.sh
@@ -29,6 +29,7 @@ for t in "$@"; do
   [[ $N -eq ${BASH_REMATCH[2]} ]] || abort
   [[ $S -eq ${BASH_REMATCH[3]} ]] || abort
   sum1=${BASH_REMATCH[8]}
+  time1=${BASH_REMATCH[9]}
 
   # Clean BASH_REMATCH just in case
   [[ 0 =~ 0 ]]
@@ -38,12 +39,13 @@ for t in "$@"; do
   [[ $N -eq ${BASH_REMATCH[2]} ]] || abort
   [[ $S -eq ${BASH_REMATCH[3]} ]] || abort
   sum2=${BASH_REMATCH[8]}
+  time2=${BASH_REMATCH[9]}
 
   if [[ $sum -ne $ref ]]; then
     echo -e "BAD\tinput: ($M,$N,$S) result: $sum expected: $ref"
     errors=$(($errors + 1))
   else
-    echo -e "OK\tinput: ($M,$N,$S)"
+    echo -e "OK\tinput: ($M,$N,$S) times: $time1 | $time2 (`echo $time1 - $time2 | bc`) "
   fi
 done
 
