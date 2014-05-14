e4ff598401bfbdd4e81228a06599fa568955f69e
Mateusz Machalica <mateuszmachalica@gmail.com>
2014-05-15 01:08:26 +0200

    Fixed regex for parsing output.

--- correctness/seq-0.sh
+++ correctness/seq-0.sh
@@ -3,7 +3,7 @@
 COMPARE="`dirname $0`/../compare-seq.sh"
 
 RANDOM=123123
-COUNT=1000
+COUNT=10000
 MAX_M=50
 MAX_N=50
 
@@ -11,9 +11,11 @@ errors=0
 for i in `seq $COUNT`; do
   echo -ne "kadane\t"
   $COMPARE naive kadane "($(($RANDOM % $MAX_M + 1)),$(($RANDOM % $MAX_N + 1)),$RANDOM)"
+  [[ $? -eq 0 ]] || errors=$(($errors + 1))
   echo -ne "takaoka\t"
   $COMPARE naive takaoka "($(($RANDOM % $MAX_M + 1)),$(($RANDOM % $MAX_N + 1)),$RANDOM)"
-  if [[ $? -ne 0 ]]; then
-    errors=$(($errors + 1))
-  fi
+  [[ $? -eq 0 ]] || errors=$(($errors + 1))
 done
+
+echo "TOTAL ERRORS: $errors"
+exit $errors
