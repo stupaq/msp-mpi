d85ebd0233ee2f6585378b59b85d22c2ea4942b0
Mateusz Machalica <mateuszmachalica@gmail.com>
2014-05-14 12:05:39 +0200

    Sequential programs cross verifier.

--- commons.sh
+++ commons.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 RESULT_LINE_REGEX='Input: \(([0-9]*),([0-9]*),([0-9]*)\) Solution: \|\(([0-9]*),([0-9]*)\),\(([0-9]*),([0-9]*)\)\|=([.,0-9]*) Time: ([.,0-9]*)'
+TEST_CASE_REGEX='\(([0-9]*),([0-9]*),([0-9]*)\)'
 
 function abort() {
   echo "ERROR: ${1:-aborting due to unexpected error}"
---
+++ compare-seq.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+source "`dirname $0`/commons.sh"
+
+if [[ $# -lt 2 ]]; then
+  echo "Must provide two binaries for cross-verification."
+  exit 1
+fi
+
+BIN1="${1}"
+if [[ ! -x "$BIN1" ]]; then
+  BIN1="./msp-seq-$BIN1.exe"
+fi
+BIN2="${2}"
+if [[ ! -x "$BIN2" ]]; then
+  BIN2="./msp-seq-$BIN2.exe"
+fi
+
+shift 2
+
+errors=0
+for t in "$@"; do
+  [[ $t =~ $TEST_CASE_REGEX ]] || abort "cannot match test case"
+  M=${BASH_REMATCH[1]}
+  N=${BASH_REMATCH[2]}
+  S=${BASH_REMATCH[3]}
+
+  [[ `$BIN1 $M $N $S 2>&1` =~ $RESULT_LINE_REGEX ]] || abort "cannot match output 1"
+  [[ $M -eq ${BASH_REMATCH[1]} ]] || abort
+  [[ $N -eq ${BASH_REMATCH[2]} ]] || abort
+  [[ $S -eq ${BASH_REMATCH[3]} ]] || abort
+  sum1=${BASH_REMATCH[8]}
+
+  # Clean BASH_REMATCH just in case
+  [[ 0 =~ 0 ]]
+
+  [[ `$BIN2 $M $N $S 2>&1` =~ $RESULT_LINE_REGEX ]] || abort "cannot match output 2"
+  [[ $M -eq ${BASH_REMATCH[1]} ]] || abort
+  [[ $N -eq ${BASH_REMATCH[2]} ]] || abort
+  [[ $S -eq ${BASH_REMATCH[3]} ]] || abort
+  sum2=${BASH_REMATCH[8]}
+
+  if [[ $sum -ne $ref ]]; then
+    echo -e "BAD\tinput: ($M,$N,$S) result: $sum expected: $ref"
+    errors=$(($errors + 1))
+  else
+    echo -e "OK\tinput: ($M,$N,$S)"
+  fi
+done
+
+exit $errors
