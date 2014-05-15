93d6fd1d0ed77b22c8d4720407deaf186767578f
Mateusz Machalica <mateuszmachalica@gmail.com>
2014-05-15 09:44:10 +0200

    Fixed the zero seed bug.

--- correctness/seq-0.sh
+++ correctness/seq-0.sh
@@ -9,11 +9,14 @@ MAX_N=50
 
 errors=0
 for i in `seq $COUNT`; do
+  M=$(($RANDOM % $MAX_M + 1))
+  N=$(($RANDOM % $MAX_N + 1))
+  S=$(($RANDOM + 1))
   echo -ne "kadane\t"
-  $COMPARE naive kadane "($(($RANDOM % $MAX_M + 1)),$(($RANDOM % $MAX_N + 1)),$RANDOM)"
+  $COMPARE naive kadane "($M,$N,$S)"
   [[ $? -eq 0 ]] || errors=$(($errors + 1))
   echo -ne "takaoka\t"
-  $COMPARE naive takaoka "($(($RANDOM % $MAX_M + 1)),$(($RANDOM % $MAX_N + 1)),$RANDOM)"
+  $COMPARE naive takaoka "($M,$N,$S)"
   [[ $? -eq 0 ]] || errors=$(($errors + 1))
 done
 
