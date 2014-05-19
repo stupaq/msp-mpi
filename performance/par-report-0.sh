1aeeb0f5d46775d783de60fd1a158df7c6cd3639
Mateusz Machalica <mateuszmachalica@gmail.com>
2014-05-19 13:05:08 +0200

    Performance evaluation for 4000x4000 for P < 16.

--- performance/par-report-0.sh
+++ performance/par-report-0.sh
@@ -3,7 +3,7 @@
 for p in 32 16 8 4; do
   for m in 50 100 200 250 400 1000 4000 8000; do
     for n in 50 100 200 250 400 1000 4000 8000; do
-      if [[ $p -ge 16 || ( $m -le 1000 && $n -le 1000 ) ]]; then
+      if [[ $p -ge 16 || ( $m -le 4000 && $n -le 4000 ) ]]; then
         for s in 2347 4412343 9247 2435 4234; do
           ./submit-par.sh -m $m -n $n -s $s -p $p -A -s
         done
--- performance/par-report-1.sh
+++ performance/par-report-1.sh
@@ -4,7 +4,7 @@ for p in 32 16 8 4; do
   for c in 1 2; do
     for m in 50 1000 4000 8000; do
       for n in 50 1000 4000 8000; do
-        if [[ $p -ge 16 || ( $m -le 1000 && $n -le 1000 ) ]]; then
+        if [[ $p -ge 16 || ( $m -le 4000 && $n -le 4000 ) ]]; then
           for s in 2347 4412343 9247 2435 4234; do
             ./submit-par.sh -m $m -n $n -s $s -p $p -c $c -A -s
           done
