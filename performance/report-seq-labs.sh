f266a1b3b17e5bbc89fd1f1be143a3bad6c689fe
Mateusz Machalica <mateuszmachalica@gmail.com>
2014-05-20 18:48:29 +0200

    Parallelized comparison between takaoka and kadane (sequential versions).

--- performance/report-seq-labs.sh
+++ performance/report-seq-labs.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 
 for s in 2347 2435 4234 4412343 9247; do
-  for m in 1000 4000 8000; do
-    for n in 1000 4000 8000; do
-      ./run-seq.sh -m $m -n $n -s $s -v kadane -M reports/
-    done
-  done
-
-  for m in 1000 4000; do
-    for n in 1000 4000; do
-      ./run-seq.sh -m $m -n $n -s $s -v takaoka -M reports/
-    done
-  done
+  ./run-seq.sh -m 1000 -n 1000 -s $s -v kadane -M reports/ &
+  ./run-seq.sh -m 1000 -n 4000 -s $s -v kadane -M reports/ &
+  ./run-seq.sh -m 1000 -n 8000 -s $s -v kadane -M reports/ &
+  ./run-seq.sh -m 4000 -n 1000 -s $s -v kadane -M reports/ &
+  wait
+  ./run-seq.sh -m 4000 -n 4000 -s $s -v kadane -M reports/ &
+  ./run-seq.sh -m 4000 -n 8000 -s $s -v kadane -M reports/ &
+  ./run-seq.sh -m 8000 -n 1000 -s $s -v kadane -M reports/ &
+  ./run-seq.sh -m 8000 -n 4000 -s $s -v kadane -M reports/ &
+  wait
+  ./run-seq.sh -m 8000 -n 8000 -s $s -v kadane -M reports/ &
+  ./run-seq.sh -m 1000 -n 1000 -s $s -v takaoka -M reports/ &
+  ./run-seq.sh -m 1000 -n 4000 -s $s -v takaoka -M reports/ &
+  ./run-seq.sh -m 4000 -n 1000 -s $s -v takaoka -M reports/ &
+  wait
+  ./run-seq.sh -m 4000 -n 4000 -s $s -v takaoka -M reports/ &
+  wait
 done
 
