f30c18bcf0c69111f2b19bc90d4a1db4f0be6865
Mateusz Machalica <mateuszmachalica@gmail.com>
2014-05-21 16:41:15 +0200

    Full range of P.

--- performance/report-par-square.sh
+++ performance/report-par-square.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
 for s in 2347 2435 4234 4412343 9247; do
-  for w in 16 8 4 2 1; do
+  for w in 64 32 16 8 4 2 1; do
     for c in 4 2 1; do
-      for m in 250 400 1000 4000; do
-        for n in 250 400 1000 4000; do
-          ./submit-par.sh -m $m -n $n -s $s -p $(($w * $c)) -c $c -A -s -M reports/
+      if [[ $(($w * $c)) -le 64 ]]; then
+        for m in 250 400 1000 4000; do
+          for n in 250 400 1000 4000; do
+            ./submit-par.sh -m $m -n $n -s $s -p $(($w * $c)) -c $c -A -s -M reports/
+          done
         done
-      done
+      fi
     done
   done
 done
